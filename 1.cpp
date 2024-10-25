04b54b1bfd71c2fc75b4d11d64f705e19c5cabd3
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-10-25 21:37:17 +0700

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,a[15],final=0;
+void ktao(){
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        a[i]=0;
+    }
+}
+void sinh(){
+    int i=n;
+    while(i>0&&a[i]==1){
+        a[i]=0;
+        i--;
+    }
+    if(i==0)final=1;
+    else{
+        a[i]=1;
+    }
+}
+int main(){
+        ktao();
+        final=0;
+        while(final==0){
+        for(int i=1;i<=n;i++){
+            if(a[i]==1)cout<<"1";
+            else cout<<"0";
+        }
+        cout<<endl;
+        sinh();
+        }
+        cout<<endl;
+}
